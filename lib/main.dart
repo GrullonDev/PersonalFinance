d263a4d4317a44f423609bf70445fa213064e02f
Jorge Luis Grullón Marroquín <jorgegrullon@Mac.ht.home>
2025-07-28 22:29:17 -0600

    Refactor import statements for better organization and streamline Firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,20 @@
 import 'dart:ui' as ui;
 
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:intl/intl.dart';
+
+import 'package:personal_finance/features/alerts/alert_item.dart';
 import 'package:personal_finance/features/data/model/expense.dart';
 import 'package:personal_finance/features/data/model/income.dart';
-import 'package:personal_finance/features/alerts/alert_item.dart';
 import 'package:personal_finance/utils/app.dart';
 import 'package:personal_finance/utils/injection_container.dart';
 import 'package:personal_finance/utils/pending_action.dart';
 
-import 'utils/offline_sync_service.dart';
 import 'firebase_options.dart';
+import 'utils/offline_sync_service.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -35,9 +37,7 @@ Future<void> main() async {
   await OfflineSyncService().init();
 
   // Inicializa Firebase con opciones por plataforma
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   // Configura dependencias
   await initDependencies();
