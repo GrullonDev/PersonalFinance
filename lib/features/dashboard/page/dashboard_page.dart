22fe4c25582a684e17eccfa0146a34f35cabc8c9
Grullon Dev <prosystem155@gmail.com>
2025-11-01 22:25:23 -0600

    chore: remove old dashboard structure

--- lib/features/dashboard/page/dashboard_page.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-import 'package:personal_finance/features/dashboard/page/dashboard_layout_v2.dart';
-
-class DashboardPage extends StatelessWidget {
-  const DashboardPage({super.key});
-  @override
-  Widget build(BuildContext context) => const DashboardLayoutV2();
-}
