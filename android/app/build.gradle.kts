6aeb9134e6a85d5da295aacb7027185d3007fa46
GrullonDev <prosystem155@gmail.com>
2026-02-26 13:07:27 -0600

    build: Enable core library desugaring and add its dependency.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -37,6 +37,7 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
@@ -74,6 +75,7 @@ android {
 }
 
 dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
     implementation("com.facebook.android:facebook-android-sdk:18.0.3")
     implementation("com.google.android.gms:play-services-auth:21.0.0")
     implementation("androidx.multidex:multidex:2.0.1")
