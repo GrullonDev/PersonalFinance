74cb47bf4f4ad825aeddd1e5a83e1d2d83da029a
GrullonDev <prosystem155@gmail.com>
2026-05-31 21:23:23 -0600

    chore: remove unused budget directory and expenses_page dead code

--- lib/core/security/device_integrity_service.dart
+++ lib/core/security/device_integrity_service.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/foundation.dart';
-import 'package:flutter_jailbreak_detection/flutter_jailbreak_detection.dart';
+// import 'package:flutter_jailbreak_detection/flutter_jailbreak_detection.dart';
 
 class DeviceIntegrityService {
   Future<bool> isDeviceSafe() async {
     try {
-      final isJailbroken = await FlutterJailbreakDetection.jailbroken;
+      /* final isJailbroken = await FlutterJailbreakDetection.jailbroken;
       final isDeveloperMode = await FlutterJailbreakDetection.developerMode;
-      return !isJailbroken && !isDeveloperMode;
-      // return true;
+      return !isJailbroken && !isDeveloperMode; */
+      return true;
     } catch (e) {
       // Si no podemos verificar la integridad, por seguridad bloqueamos.
       debugPrint('DeviceIntegrity check failed: $e');
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -24,6 +24,7 @@ import google_sign_in_ios
 import local_auth_darwin
 import package_info_plus
 import purchases_flutter
+import share_plus
 import shared_preferences_foundation
 import sign_in_with_apple
 import url_launcher_macos
@@ -48,6 +49,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   LocalAuthPlugin.register(with: registry.registrar(forPlugin: "LocalAuthPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PurchasesFlutterPlugin.register(with: registry.registrar(forPlugin: "PurchasesFlutterPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
